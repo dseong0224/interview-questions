7b103d96bd1ab01905ea178fc13a33055c181ddc
dseong0224 <dseong0224@gmail.com>
2019-10-20 23:36:04 +0000

    this one's hard

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -7,21 +7,17 @@
 
 function duplicateEncode(word){
   var wordArray = word.split(''); //['h','e','l','l','o']
-  var count = -1;
-  for(var comparator = 0 ; comparator < wordArray.length; comparator++){
-  
-    for(var currentChar = 0; currentChar < wordArray.length; currentChar++ ){
-      if(wordArray[comparator] === wordArray[currentChar]){
-        count++;
-      }
-      if(count > 1){
-        wordArray[comparator] = ')'
+  for(var currentChar = 0; currentChar < wordArray.length; currentChar++){
+
+    for(var comparator = 0 ; comparator < wordArray.length; comparator++){
+      var indexOfCurrentChar = wordArray.indexOf(wordArray[currentChar])
+      if(wordArray[comparator] === wordArray[indexOfCurrentChar]){
+        console.log("match", wordArray[comparator])
+        wordArray[comparator] = '('
       }
     }
-
-    
   }
-  comparator++
+  
   var answer = wordArray.join('');
   console.log("answer", answer);
   return answer;
