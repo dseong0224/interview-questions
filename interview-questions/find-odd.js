3ca5979517cb5cfb3800e88919a31bf2530afe41
dseong0224 <dseong0224@gmail.com>
2019-10-24 07:00:01 +0000

    find odd

---
+++ interview-questions/find-odd.js
@@ -0,0 +1,20 @@
+// Given an array, find the int that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+  var answer
+  for(var index = 0; index < A.length; index++){
+    var repeat = 0;
+    for(var comparator = 0; comparator < A.length; comparator++){
+      if(A[index] === A[comparator]){
+        repeat++;
+      }
+    }
+    if(repeat%2 === 1){
+      answer = A[index]
+    }
+  }
+  return answer;
+}
+
+findOdd([1,1,1,1,1,1,1,4,4,5,5,6,6,7,7]) 
